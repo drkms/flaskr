c484b1e0e55f8dd7e3862f6f9dd90fc992e30445
drkms <dr@kms.io>
2020-04-18 20:35:12 +0200

    Cleaning init by removing hello page

--- app/__init__.py
+++ app/__init__.py
@@ -23,11 +23,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     from . import db
     db.init_app(app)
 
@@ -36,7 +31,7 @@ def create_app(test_config=None):
 
     from . import blog
     app.register_blueprint(blog.bp)
-#    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
